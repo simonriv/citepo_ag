ccf23939a1c9276a2abe4a3da00e76ffb02288c3
davidsonrave <davidsonrave1@gmail.com>
2022-07-12 16:56:47 -0500

    agregando datos personales al form

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <link rel="stylesheet" href="./resources/css/index.css" type="text/css">
-   
+
     <title>Registro Citepo</title>
 </head>
 
@@ -29,31 +29,45 @@
                 <input type="email" class="form-control" id="Email" name="email" placeholder="Ingrese el correo electronico ">
             </div>
             <div class="col-md-12">
+                <label for="ID" class="form-label">fecha de nacimiento</label>
+                <input type="date" class="form-control" id="DateBirth" name="DateBirth" placeholder="Ingrese la fecha de nacimiento ">
+            </div>
+            <div class="col-md-12">
                 <label for="ID" class="form-label">Numero de Identificacion</label>
                 <input type="number" class="form-control" id="ID" name="id" placeholder="Ingrese el numero de identificacion">
             </div>
+
+
             <div class="col-12">
                 <label for="phone" class="form-label">Telefono</label>
                 <input type="tel" class="form-control" id="phone" name="tel" placeholder="Ingrese el numero de Telefono ">
             </div>
+            <div class="col-12">
+            <label for="countries" class="form-label">Pais</label>
+                    <select class="form-select form-select-sm" id="countries" name="countries aria-label=".form-select-sm example">
+                        
+                       
+                    </select>
 
+                </div>
             <div class="col-12">
                 <label for="occupation" class="form-label">Ocupacion</label>
                 <div class="form-check">
                     <input type="radio" class="form-check-input" id="validationFormCheck2" name="radio-tecnico" required>
                     <label class="form-check-label" for="validationFormCheck2">Técnico</label>
                 </div>
+               
                 <div class="form-check mb-3">
                     <input type="radio" class="form-check-input" id="validationFormCheck3" name="radio-notecnico" required>
                     <label class="form-check-label" for="validationFormCheck3">No soy Técnico</label>
-                    
+
                 </div>
             </div>
 
 
 
             <div class="col-12">
-                <button type="submit" class="btn btn-primary">Registrar</button>
+                <button type="submit" class="btn btn-primary" id="btn">Registrar</button>
             </div>
         </form>
 
@@ -63,4 +77,5 @@
 </body>
 <script src="./resources/js/index.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+
 </html>
