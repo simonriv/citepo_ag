da6b8bb9ef133731a7958761e3f000a73e73a8a4
Simon Rivera <tutorialessimon@gmail.com>
2022-07-15 14:35:40 -0500

    Funcion para registrar usuario

---
+++ app/library/functions.php
@@ -0,0 +1,69 @@
+<?php
+date_default_timezone_set("America/Bogota");
+
+require_once $_SERVER['DOCUMENT_ROOT'].'/app/config/connection.php';
+
+class Functions extends Connection{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    private function lastInsertedId($table,$field_name){
+        try{
+            $q = "SELECT COUNT(*) AS total FROM $table";
+            $query = $this->connect()->query($q);
+            $total = $query->fetch(PDO::FETCH_OBJ)->total;
+            
+            if($total > 0){
+                $q = "SELECT MAX($field_name) AS id FROM $table";
+                $query = $this->connect()->query($q);
+                $id = $query->fetch(PDO::FETCH_OBJ)->id;
+                return $id;
+            }else {
+                return 0;
+            }
+        }catch(Exception $e){
+            return '003' . 'Error al consultar el ultimo id insertado: '.$e->getMessage();
+        }
+    }
+
+    private function checkExistance($email){
+        try{
+            $q = "SELECT COUNT(*) AS total FROM user WHERE email like '$email'";
+            $query = $this->connect()->query($q);
+            $existance = $query->fetch(PDO::FETCH_OBJ)->total;
+
+            if($existance > 0){
+                return true;
+            }else {
+                return false;
+            }
+
+        }catch(Exception $e){
+            return "Error 003" . $e;
+        }
+    }
+
+    public function register($name,$last_name,$email,$dni,$zip_phone,$phone,$country,$state,$city,$ocupation,$area,$birthdate,$stauts,$pdf){
+        try {
+            if($this->checkExistance($email) !== "Error" && $this->checkExistance($email) == false){
+                $q = "INSERT INTO client (Name,LastName,Email,DNI,PhoneZip,Phone,Country,State,City,Ocupation,Area,BirthDate,Status,Pdf) VALUES 
+                (:name,:last_name,:email,:dni,:zip_phone,:phone,:country,:state,:city,:ocupation,:area,:birthdate,:stauts,:pdf)";
+                $query = $this->connect()->prepare($q);
+                $query->execute(['name' => $name,'last_name' => $last_name,'email' => $email, 'dni' => $dni,'zip_phone' => $zip_phone, 'phone' => $phone, 'country' => $country, 'state' => $state, 'city' => $city, 'ocupation' => $ocupation, 'area' => $area, 'birthdate' => $birthdate,'status' => $stauts, 'pdf' => $pdf]);
+                
+                return $this->lastInsertedId("client","ClientId");
+            }else {
+                return 0;
+            }
+        }catch(PDOException $e){
+            return "Error 001" . $e;
+        }
+    } 
+
+
+}
+
+
+?>
